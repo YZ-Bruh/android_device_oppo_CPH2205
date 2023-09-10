4125c301da3422f17db15ea2b5cc702c2901cf56
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-09-10 09:56:40 +0300

    omni > twrp

--- twrp_CPH2205.mk
+++ twrp_CPH2205.mk
@@ -10,20 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omn/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from CPH2205 device
 $(call inherit-product, device/oppo/CPH2205/device.mk)
 
 PRODUCT_DEVICE := CPH2205
-PRODUCT_NAME := omni_CPH2205
+PRODUCT_NAME := twrp_CPH2205
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := CPH2205
 PRODUCT_MANUFACTURER := oppo
-
-PRODUCT_GMS_CLIENTID_BASE := android-oppo
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_oppo6779-user 11 RP1A.200720.011 1614996321774 release-keys"
-
-BUILD_FINGERPRINT := alps/vnd_oppo6779/oppo6779:11/RP1A.200720.011/1614996321774:user/release-keys
