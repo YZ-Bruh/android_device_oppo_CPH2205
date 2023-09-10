faae076723454c51432e928eaea9b1796c6c207f
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-09-10 09:57:21 +0300

    omni > twrp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_CPH2205-user
-add_lunch_combo omni_CPH2205-userdebug
-add_lunch_combo omni_CPH2205-eng
+add_lunch_combo twrp_CPH2205-user
+add_lunch_combo twrp_CPH2205-userdebug
+add_lunch_combo twrp_CPH2205-eng
